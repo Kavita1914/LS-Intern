fd783653d870f596f294e90bafc071d3b36caf1d
Kavita Kumari <chauhan.kavita2248@gmail.com>
2024-07-11 12:58:16 +0530

    flatten array consisting of nested array

--- problemSolving/problemSolving1.js
+++ problemSolving/problemSolving1.js
@@ -11,31 +11,6 @@
 //   3. Filter out the students who have an average score below 80
 
 
-//   An array of product objects, each with the following structure:
-//   const products = [
-//     { id: 1, name: 'Laptop', price: 1000 },
-//     { id: 2, name: 'Phone', price: 500 },
-//     { id: 3, name: 'Tablet', price: 750 },
-//     { id: 4, name: 'Monitor', price: 300 },
-//     { id: 5, name: 'Headphones', price: 150 }
-//   ];
-  
-//   An array of order objects, each with the following structure:
-  
-  // const orders = [
-  //   { orderId: 1, customerId: 1, items: [{ productId: 1, quantity: 1 }, { productId: 2, quantity: 2 }] },
-  //   { orderId: 2, customerId: 2, items: [{ productId: 3, quantity: 1 }] },
-  //   { orderId: 3, customerId: 1, items: [{ productId: 4, quantity: 2 }, { productId: 5, quantity: 1 }] },
-  //   { orderId: 4, customerId: 3, items: [{ productId: 2, quantity: 3 }, { productId: 5, quantity: 2 }] }
-  // ];
-  
-//   Your tasks are:
-  
-//   Calculate the total value of each order.
-//   Identify the customer who spent the most money.
-//   Find the most frequently ordered product.
-//   List all customers who ordered a specific product (e.g., product with ID 2).
-
   const students = [
     { name: 'Alice', scores: [95, 85, 90] },
     { name: 'Bob', scores: [70, 75, 80] },
